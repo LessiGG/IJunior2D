9a073f6689333a5d76719e6e8076074ec52cb016
LessiGG <ivan-lesnichenko@mail.ru>
2022-09-04 17:25:39 +0300

    Initial commit

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private float _volumeIncreaseSpeed;
+    
+    private bool _isTurned;
+    private float _minAlarmVolume = 0;
+    private float _maxAlarmVolume = 1;
+    
+    private AudioSource _audioSource;
+    private Coroutine _coroutine;
+    
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TurnOnAlarm()
+    {
+        _isTurned = true;
+        OnAlarmSwitch();
+    }
+
+    public void TurnOffAlarm()
+    {
+        _isTurned = false;
+        OnAlarmSwitch();
+    }
+
+    private void OnAlarmSwitch()
+    {
+        if (_isTurned)
+        {
+            _coroutine = StartCoroutine(ChangeVolume(_maxAlarmVolume));
+        }
+        else
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = StartCoroutine(ChangeVolume(_minAlarmVolume));
+        }
+    }
+
+    private IEnumerator ChangeVolume(float target)
+    {
+        while (_audioSource.volume != target)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, target, _volumeIncreaseSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }   
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Door : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private UnityEvent _entered;
+    [SerializeField] private UnityEvent _exited;
+
+    private bool _isEntered;
+
+    private void EnterHouse()
+    {
+        _isEntered = true;
+        _entered?.Invoke();
+    }
+    
+    private void ExitHouse()
+    {
+        _isEntered = false;
+        _exited?.Invoke();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_isEntered == false)
+            EnterHouse();
+        else
+            ExitHouse();
+    }
+}
