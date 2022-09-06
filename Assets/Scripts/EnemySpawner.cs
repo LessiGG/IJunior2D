bd8f12e76b5a53cd8066680729cec81e1fd6e72a
LessiGG <ivan-lesnichenko@mail.ru>
2022-09-06 18:23:38 +0300

    Spawners

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Spawner[] _spawners;
+
+    private float _spawnDelay = 2f;
+    
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    
+    private IEnumerator Spawn()
+    {
+        foreach (var spawner in _spawners)
+        {
+            spawner.Spawn();
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _prefab;
+
+    public void Spawn()
+    {
+        Instantiate(_prefab, transform.position, quaternion.identity);
+    }
+}
