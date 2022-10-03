8fdc10801c1f43d88920f97c78a4aba2fb245ebb
LessiGG <72992800+LessiGG@users.noreply.github.com>
2022-10-03 12:43:20 +0300

    Add files via upload

---
+++ Uzi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class Uzi : Weapon
+    {
+        [SerializeField] private int _bulletsCount;
+        [SerializeField] private float _fireRateDelay;
+        
+        public override void Shoot(Transform shootPoint)
+        {
+            StartCoroutine(UziShoot(shootPoint));
+        }
+
+        private IEnumerator UziShoot(Transform shootPoint)
+        {
+            var fireRateDelay = new WaitForSeconds(_fireRateDelay);
+
+            for (int i = 0; i < _bulletsCount; i++)
+            {
+                Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+                yield return fireRateDelay;
+            }
+        }
+    }
+}
